1781477466f02a6d3a047e51cadc6cbb1dcb1ab1
chixucx <chi_xu_1016@163.com>
2018-08-12 20:06:26 +0800

    图片验证码后端逻辑实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,7 +1,30 @@
+import logging
+from flask import abort
+from flask import make_response
 from flask import request
+from info import constants
+from info import redis_store
+from utils.captcha.captcha import captcha
 from . import passport_blue
 
 
-@passport_blue.route('/passport')
-def passport(imageCode):
-    pass
+@passport_blue.route('/image_code')
+def get_image_code():
+    # 接收图片uuid
+    image_code_id = request.args.get("imageCodeId")
+    # 校验uuid
+    if not image_code_id:
+        abort(400)
+    # 生成验证码的图片和文字信息
+    name, text, image = captcha.instance()
+    # 讲uuid和图片验证码文字绑定到redis
+    try:
+        redis_store.set("imageCode:" + image_code_id, text, constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        logging.error(e)
+        abort(500)
+    # 响应
+    response = make_response()
+    response.header['Content-Type'] = 'image/jpg'
+    return response
+
