86628d932b5fe8fa392d5de7ac10ea4d382a7689
chixucx <chi_xu_1016@163.com>
2018-08-12 18:27:12 +0800

    准备主页之导入静态文件显示主页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from . import index_blue
 
 
 @index_blue.route('/', methods=['GET', 'POST'])
 def index():
-    return 'index'
+    return render_template('news/index.html')
