960b39df44d08ff4b65532448d65362bff565597
chixucx <chi_xu_1016@163.com>
2018-08-14 19:43:13 +0800

    右上角用户名

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,11 +1,26 @@
-from flask import render_template,current_app
-
+import logging
+from flask import render_template, current_app
+from flask import session
+from info.models import User
 from . import index_blue
 
 
 @index_blue.route('/')
 def index():
-    return render_template('news/index.html')
+    # 从session查询user_id
+    user_id = session.get('user_id', None)
+    # 用user_id在数据库中查询user对象
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            logging.error(e)
+    # 构造渲染模板的上下文
+    context = {
+        'user': user.to_dict() if user else None
+    }
+    return render_template('news/index.html', context=context)
 
 
 @index_blue.route('/favicon.ico')
